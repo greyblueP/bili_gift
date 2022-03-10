c85d4a1f7613927e5023c0687f7946afb7891816
greyblueP <92810307+greyblueP@users.noreply.github.com>
2022-03-10 21:42:39 +0800

    Update 礼物图标获取.py

--- 礼物图标获取.py
+++ 礼物图标获取.py
@@ -5,7 +5,7 @@ import json
 
 
 # 读取旧数据
-def get():
+def get_data():
     list_old = []
     try:
         with open('礼物列表.json', 'r')as f:
@@ -16,23 +16,9 @@ def get():
         return False
 
 
-# 网页源码读取
-def online(url):
-    headers = {
-        'authority': 'api.live.bilibili.com',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36 Edg/90.0.818.56',
-    }
-    url = url
-    req = request.Request(url=url, headers=headers)
-    html = request.urlopen(req)  # 打开指定网址
-    html = html.read()  # 读取网页源码
-    html = html.decode('utf8')  # 解码
-    print('已连接到网站')
-    return html
-
-
 # 读取新数据
-def txt(html):
+def txt(url):
+    html = get(url).text
     pat_1 = re.compile(r'{"id":\d{1,20},"name":"(.*?)","price"')
     pat1 = pat_1.findall(html)
     pat_2 = re.compile(r'"img_basic":"(.*?)","img_dynamic"')
@@ -63,7 +49,13 @@ def check(list_new):
                     repeat[png].add(j)
                 else:
                     repeat[png] = {i, j}
-    return repeat
+                break
+    if len(repeat) != 0:
+        print('下载地址重复的礼物:')
+        for i in repeat:
+            print('  '+i)
+            print('    '+str(repeat[i]))
+        print('\n')
 
 
 # 新旧数据对比
@@ -72,7 +64,12 @@ def find(list_old, list_new):
     for i in list_new:
         if i not in list_old:
             new_gift.append(i)
-    return new_gift
+    if len(new_gift) != 0:
+        print('本次更新发现'+str(len(new_gift))+'个新礼物:')
+        for i in new_gift:
+            print('  '+i)
+    else:
+        print('本次更新未发现新礼物')
 
 
 # 下载礼物
@@ -89,7 +86,7 @@ def download(list_new):
             print(gift+'.gif', '下载成功')
         except:
             print('下载失败,请手动下载')
-            print(url['PNG'], '\n', url['GIF'])
+            print(url['PNG']+'\n'+url['GIF'])
     else:
         print('未找到指定名称的礼物,请检查输入后再下载')
 
@@ -103,31 +100,18 @@ def download(list_new):
 
 print('b站礼物获取v1.2\n')
 # 读取旧数据
-list_old = get()
+list_old = get_data()
 # 读取新数据
 url = 'https://api.live.bilibili.com/gift/v3/live/gift_config'
-html = online(url)
-list_new = txt(html)
+list_new = txt(url)
 # 写入新数据
 with open('礼物列表.json', 'w')as f:
     json.dump(list_new, f, indent=4, ensure_ascii=False)
 # 检测重复
 repeat = check(list_new)
-if len(repeat) != 0:
-    print('下载地址重复的礼物:')
-    for i in repeat:
-        print('  '+i)
-        print('    '+str(repeat[i]))
 # 新旧数据对比
 if list_old != False:
     new_gift = find(list_old, list_new)
-    if len(new_gift) != 0:
-        print('\n本次更新发现'+str(len(new_gift))+'个新礼物:')
-        for i in new_gift:
-            print('  '+i)
-    else:
-        print('\n本次更新未发现新礼物')
 # 下载礼物
 while True:
     download(list_new)
-
