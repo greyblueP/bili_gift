7e7c11fb5742307d1a3dc9b119a416ffe867031a
greyblueP <92810307+greyblueP@users.noreply.github.com>
2023-02-08 13:31:57 +0800

    Update 礼物图标获取.py
    功能增加

--- 礼物图标获取.py
+++ 礼物图标获取.py
@@ -1,7 +1,8 @@
-import re
-from urllib import request
+from re import compile
+from urllib.request import urlretrieve
 from requests import get
-import json
+from json import dump, load
+from fuzzywuzzy.process import extract
 
 
 # 读取旧数据
@@ -9,7 +10,7 @@ def get_data():
     list_old = []
     try:
         with open('礼物列表.json', 'r')as f:
-            list_old = json.load(f)
+            list_old = load(f)
         return list_old
     except:
         print('未找到文件 礼物列表.json\n')
@@ -19,12 +20,9 @@ def get_data():
 # 读取新数据
 def txt(url):
     html = get(url).text
-    pat_1 = re.compile(r'{"id":\d{1,20},"name":"(.*?)","price"')
-    pat1 = pat_1.findall(html)
-    pat_2 = re.compile(r'"img_basic":"(.*?)","img_dynamic"')
-    pat2 = pat_2.findall(html)
-    pat_3 = re.compile(r'"gif":"(.*?)","webp"')
-    pat3 = pat_3.findall(html)
+    pat1 = compile(r'{"id":\d{1,20},"name":"(.*?)","price"').findall(html)
+    pat2 = compile(r'"img_basic":"(.*?)","img_dynamic"').findall(html)
+    pat3 = compile(r'"gif":"(.*?)","webp"').findall(html)
 
     list_new = {}
     for i in range(len(pat1)):
@@ -37,7 +35,7 @@ def txt(url):
 
 
 # 检测重复
-def check(list_new):
+def check():
     repeat = {}
     for i in list_new:
         for j in list_new:
@@ -53,42 +51,58 @@ def check(list_new):
     if len(repeat) != 0:
         print('下载地址重复的礼物:')
         for i in repeat:
-            print('  '+i)
-            print('    '+str(repeat[i]))
+            print(' '*2+i)
+            print(' '*4+str(repeat[i]))
         print('\n')
 
 
 # 新旧数据对比
-def find(list_old, list_new):
+def find():
     new_gift = []
-    for i in list_new:
-        if i not in list_old:
-            new_gift.append(i)
+    for gift in list_new:
+        if gift not in list_old:
+            new_gift.append(gift)
     if len(new_gift) != 0:
         print('本次更新发现'+str(len(new_gift))+'个新礼物:')
-        for i in new_gift:
-            print('  '+i)
+        for gift in new_gift:
+            print('  '+gift)
+        Download = input('是否下载新礼物(y/n):')
+        if Download == 'y' or Download == 'Y' or Download == '是':
+            for gift in new_gift:
+                download(gift)
     else:
         print('本次更新未发现新礼物')
 
 
+# 模糊搜素，检测是否gift与list_new中的礼物名称相似
+def search(gift):
+    list = []
+    for gift in list_new:
+        list += [gift]
+    search_list = extract(gift, list, limit=5)
+    if len(search_list) != 0:
+        return search_list
+
+
 # 下载礼物
-def download(list_new):
-    gift = input('\n请输入需要下载图标的礼物名称:')
+def download(gift):
     if gift in list_new:
         url = list_new[gift]
         try:
             # png
-            request.urlretrieve(url['PNG'], gift+'.png')
+            urlretrieve(url['PNG'], gift+'.png')
             print(gift+'.png', '下载成功')
             # gif
-            request.urlretrieve(url['GIF'], gift+'.gif')
+            urlretrieve(url['GIF'], gift+'.gif')
             print(gift+'.gif', '下载成功')
         except:
             print('下载失败,请手动下载')
             print(url['PNG']+'\n'+url['GIF'])
     else:
-        print('未找到指定名称的礼物,请检查输入后再下载')
+        search_list = search(gift)
+        print(' '*2+'未找到指定名称的礼物,请问需要下载的礼物是否是:')
+        for i in search_list:
+            print(' '*4+i[0])
 
 
 # {
@@ -97,21 +111,19 @@ def download(list_new):
 #         gif:xxx
 #     }
 # }
-
-print('b站礼物获取v1.2\n')
+print('b站礼物获取v1.3\n')
 # 读取旧数据
 list_old = get_data()
 # 读取新数据
-url = 'https://api.live.bilibili.com/gift/v3/live/gift_config'
-list_new = txt(url)
+list_new = txt('https://api.live.bilibili.com/gift/v3/live/gift_config')
 # 写入新数据
 with open('礼物列表.json', 'w')as f:
-    json.dump(list_new, f, indent=4, ensure_ascii=False)
+    dump(list_new, f, indent=4, ensure_ascii=False)
 # 检测重复
-repeat = check(list_new)
+check()
 # 新旧数据对比
 if list_old != False:
-    new_gift = find(list_old, list_new)
+    find()
 # 下载礼物
 while True:
-    download(list_new)
+    download(input('\n请输入需要下载图标的礼物名称:'))
